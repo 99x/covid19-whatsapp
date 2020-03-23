5123f9b9662215af0b3a191688d0ba9d4e947308
Hasith Yaggahavita <hasithy@HasithY-NEW.local>
2020-03-23 13:24:40 +0530

    mwnu changes

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 
 def get_welcome_menu():
     menu = '''
-    Sri Lanka COVID-19 bot (by UNDP and 99XTechnology) \n\n Coronavirus information: \n කොරෝනා වයිරස් තොරතුරු:\nகொரோனா வைரஸ் தகவல்:
+    Sri Lanka COVID-19 bot \n\n Coronavirus information: \n කොරෝනා වයිරස් තොරතුරු:\nகொரோனா வைரஸ் தகவல்:
 
     *1.* Provide data in English language
     *2.* සිංහල භාෂාවෙන් දත්ත දෙන්න 
