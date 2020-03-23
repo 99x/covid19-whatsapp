d3401252adee9aac4455f0bf17861712fc15070b
Hasith Yaggahavita <hasithy@HasithY-NEW.local>
2020-03-23 12:05:30 +0530

    code cleanup

--- menu.py
+++ menu.py
@@ -12,30 +12,3 @@ def get_welcome_menu():
     *மெனு எண்ணை உள்ளிடவும்:*
     '''
     return menu
-
-def get_english_menu():
-    menu = '''
-    *Enter the menu number of the information you require*\n
-    4. Latest numbers
-    5. Related news
-    6. Graph data
-    '''
-    return menu
-
-def get_sinhala_menu():
-    menu = '''
-    *ඔබට අවශ්‍ය තොරතුරු වල මෙනු අංකය ඇතුළත් කරන්න*\n
-    7. අලුත්ම සංක්‍යා 
-    8. නවතම පුවත් 
-    9. ප්‍රස්ථාර දත්ත
-    '''
-    return menu
-
-def get_tamil_menu():
-    menu = '''
-    *உங்களுக்குத் தேவையான தகவலின் மெனு எண்ணை உள்ளிடவும்*\n
-    10. சமீபத்திய எண்கள்
-    11. சமீபத்திய செய்தி
-    12. வரைபடத் தரவு
-    '''
-    return menu
