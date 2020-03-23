6bec0193e034814509de0c63aeea19fb0bf2b443
Hasith Yaggahavita <hasithy@HasithY-NEW.local>
2020-03-23 19:08:37 +0530

    menu simplifications

--- menu.py
+++ menu.py
@@ -3,12 +3,12 @@ def get_welcome_menu():
     menu = '''
     -- SL Corona Bot -- \n\n Coronavirus information: \n කොරෝනා වයිරස් තොරතුරු:\nகொரோனா தகவல்:
 
-    *1.* In English 
-    *2.* සිංහල භාෂාවෙන්
-    *3.* தமிழ் மொழியிலிருந்து
+    1. In English 
+    2. සිංහල භාෂාවෙන්
+    3. தமிழ் மொழியிலிருந்து
     
-    *Enter the menu number:*
-    *මෙනු අංකය ඇතුළත් කරන්න:*
-    *மெனு எண்ணை உள்ளிடவும்:*
+    Enter the menu number:
+    මෙනු අංකය ඇතුළත් කරන්න:
+    மெனு எண்ணை உள்ளிடவும்:
     '''
     return menu
