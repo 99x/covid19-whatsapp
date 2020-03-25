ce1961c1849e31d2102bb9800e1087d3109a5d4b
Hasith Yaggahavita <hasithy@HasithY-NEW.local>
2020-03-25 12:05:40 +0530

    improved message by adding menu to the end of the message

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 import i18n
 
 def get_protect_info(lang):
-    menu = i18n.t('_ProtectInfo_', locale=lang)
+    menu = f'''{i18n.t('_ProtectInfo_', locale=lang)}\n\n {i18n.t('_ReplyForMenu_', locale=lang)}'''
     return menu
