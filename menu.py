1809e05afb24513cde21934e7d9979f16aca9c92
Hasith Yaggahavita <hasithy@HasithY-NEW.local>
2020-03-28 11:20:17 +0530

    smilee added to Hi

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@
 import i18n
 
 def get_welcome_menu():
-    menu = f'''Hi, \n\n{i18n.t('_Welcome_', locale="en")} \n\n{i18n.t('_Welcome_', locale="sl")}  \n\n{i18n.t('_Welcome_', locale="tm")} \n\n{i18n.t('_LangSelection_', locale="en")} \n{i18n.t('_LangSelection_', locale="sl")} \n{i18n.t('_LangSelection_', locale="tm")} '''
+    menu = f'''Hi 🙂, \n\n{i18n.t('_Welcome_', locale="en")} \n\n{i18n.t('_Welcome_', locale="sl")}  \n\n{i18n.t('_Welcome_', locale="tm")} \n\n{i18n.t('_LangSelection_', locale="en")} \n{i18n.t('_LangSelection_', locale="sl")} \n{i18n.t('_LangSelection_', locale="tm")} '''
     return menu
 
 def get_main_menu(lang):
