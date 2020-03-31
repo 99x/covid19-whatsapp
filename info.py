8ce892db5ec86a743a0c9ae6972bb2c78a214a67
Hasith Yaggahavita <hasithy@HasithY-NEW.local>
2020-03-31 23:40:40 +0530

    news sectiopn formating changes

--- info.py
+++ info.py
@@ -17,15 +17,5 @@ def get_redirect_msg():
     return info
 
 def get_latest_news(lang):
-    news = f'''*{i18n.t('_LatestNewsHeading_', locale=lang)}* \n\n
-            {i18n.t('_DeptGovInfoKey_', locale=lang)} - {i18n.t('_DeptGovInfoValue_', locale=lang)} \n
-            {i18n.t('_HPBKey_', locale=lang)} - {i18n.t('_HPBValue_', locale=lang)} \n    
-            {i18n.t('_EPIDUnitKey_', locale=lang)} - {i18n.t('_EPIDUnitValue_', locale=lang)} \n\n
-            {i18n.t('_InternationalNewsHeading_', locale=lang)} \n\n
-            {i18n.t('_SituationRepKey_', locale=lang)}: \n
-            {i18n.t('_SituationRepValue_', locale=lang)} \n\n
-            {i18n.t('_RollUpdateKey_', locale=lang)}: \n
-            {i18n.t('_RollUpdateValue_', locale=lang)} \n\n
-            {i18n.t('_NewsArticlesKey_', locale=lang)}: \n
-            {i18n.t('_NewsArticlesValue_', locale=lang)} \n\n'''
+    news = f'''*{i18n.t('_LatestNewsHeading_', locale=lang)}* \n\n{i18n.t('_DeptGovInfoKey_', locale=lang)}\n - {i18n.t('_DeptGovInfoValue_', locale=lang)} \n\n{i18n.t('_HPBKey_', locale=lang)}\n - {i18n.t('_HPBValue_', locale=lang)} \n\n{i18n.t('_EPIDUnitKey_', locale=lang)}\n - {i18n.t('_EPIDUnitValue_', locale=lang)} \n\n*{i18n.t('_InternationalNewsHeading_', locale=lang)}*\n\n{i18n.t('_SituationRepKey_', locale=lang)} \n - {i18n.t('_SituationRepValue_', locale=lang)} \n\n{i18n.t('_RollUpdateKey_', locale=lang)} \n - {i18n.t('_RollUpdateValue_', locale=lang)} \n\n{i18n.t('_NewsArticlesKey_', locale=lang)} \n - {i18n.t('_NewsArticlesValue_', locale=lang)} \n\n'''
     return news
